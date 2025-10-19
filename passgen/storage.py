77a5fe8636cd2d0da9ba7bab0f7e0f9b884caa5f
HeavenMaster300 <bogdanvarlam2@gmail.com>
2025-10-20 00:06:03 +0300

    Добавлена функция поиска паролей в storage.py

--- passgen/storage.py
+++ passgen/storage.py
@@ -24,3 +24,21 @@ def save_password(password, label, filename="passwords.json"):
     with open(filename, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
     print(f"Пароль для {label} сохранён в {filename}")
+
+def find_password(label, filename="passwords.json"):
+    """
+    Ищет хэш пароля по метке.
+    """
+    if not os.path.exists(filename):
+        print(f"Файл {filename} не найден")
+        return None
+
+    with open(filename, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    if label in data:
+        print(f"Найден хэш пароля для {label}: {data[label]['hash']}")
+        return data[label]["hash"]
+    else:
+        print(f"Пароль с меткой {label} не найден")
+        return None
